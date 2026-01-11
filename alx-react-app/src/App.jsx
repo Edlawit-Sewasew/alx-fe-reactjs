f2a1969d24e9dee7a6b1a348c236003f46c25eb9
edlawit sewasew <edlawitsewasew90@gmail.com>
2026-01-11 18:54:26 +0300

    Task 2: Create Header, MainContent, Footer and render in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,6 +7,10 @@ import Footer from './Footer';
 function App() {
   return (
     <div>
+      <Header />
+      <MainContent />
+      <Footer />
+      
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <UserProfile name="Bob" age="30" bio="Enjoys painting and traveling" />
       <UserProfile name="Charlie" age="28" bio="Fan of tech and coding" />
