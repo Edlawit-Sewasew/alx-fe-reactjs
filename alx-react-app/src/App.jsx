a3cf29a8ff647c16117fd21f20fccbb4b3a38ba7
edlawit sewasew <edlawitsewasew90@gmail.com>
2026-01-11 18:37:57 +0300

    Task 3: Add UserProfile component and render with props in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,42 +1,22 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import UserProfile from './UserProfile';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      {/* Add WelcomeMessage here */}
-      <WelcomeMessage />
+    <div>
+      
+      <Header />
+      <MainContent />
+      <Footer />
 
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+     
+      <UserProfile name="Abel" age="25" bio="Loves hiking and photography" />
+      <UserProfile name="Barkot" age="30" bio="Enjoys painting and traveling" />
+      <UserProfile name="Christian" age="28" bio="Fan of tech and coding" />
+    </div>
+  );
 }
 
-export default App
+export default App;
