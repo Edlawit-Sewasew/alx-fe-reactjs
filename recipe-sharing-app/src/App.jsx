9e077067a62e2ee8af95009771a49df38638f134
edlawit sewasew <edlawitsewasew90@gmail.com>
2026-02-01 23:01:37 +0300

    update recipe-sharing-app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,25 @@
+import { Routes, Route } from 'react-router-dom'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
+import RecipeDetails from './components/RecipeDetails'
 import './App.css'
 
 function App() {
   return (
     <>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+      </Routes>
     </>
   )
 }
---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,32 @@
+import { Link, useParams } from 'react-router-dom'
+import { useRecipeStore } from './recipeStore'
+import EditRecipeForm from './EditRecipeForm'
+import DeleteRecipeButton from './DeleteRecipeButton'
+
+const RecipeDetails = () => {
+  const { recipeId } = useParams()
+  const recipe = useRecipeStore(state =>
+    state.recipes.find(r => r.id === Number(recipeId))
+  )
+
+  if (!recipe) {
+    return (
+      <div>
+        <p>Recipe not found.</p>
+        <Link to="/">Back to recipes</Link>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Link to="/">← Back to recipes</Link>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipeId} />
+    </div>
+  )
+}
+
+export default RecipeDetails
