378f10f5e76d35ca3290cf07ba1afea3df4f061e
edlawit sewasew <edlawitsewasew90@gmail.com>
2026-02-15 17:26:27 +0300

    Refactor App component to simplify routing and integrate HomePage

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,32 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-import RecipeList from './components/RecipeList'
-import SearchBar from './components/SearchBar'
-import AddRecipeForm from './components/AddRecipeForm'
+import HomePage from './components/HomePage'
 import RecipeDetails from './components/RecipeDetails'
-import FavoritesList from './components/FavoritesList'
-import RecommendationsList from './components/RecommendationsList'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1 className="text-blue-500">Recipe Sharing App</h1>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <SearchBar />
-              <FavoritesList />
-              <RecommendationsList />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+    </Routes>
   )
 }
 
